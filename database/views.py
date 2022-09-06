1be4d898dbde9129ce528a44690155107ad8ba5f
ishakboufatah <boufatah.ishak@gmail.com>
2022-09-06 19:15:55 +0100

    first add

--- database/views.py
+++ database/views.py
@@ -10,9 +10,7 @@ import anaabkari.urls
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from rest_framework.filters import SearchFilter,OrderingFilter
 from rest_framework.permissions import DjangoModelPermissions
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 
 
